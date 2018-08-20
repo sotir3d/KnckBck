e16074ace2cf060054dfd700f27795bd13378d40
sotir3d <sebastian.fueloep@gmail.com>
2018-08-20 17:59:23 +0200

    Initial commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    Vector2 direction;
+    Quaternion playerRotation;
+    Rigidbody2D rigidBody;
+
+    float angle;
+
+    bool fired = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        playerRotation = transform.rotation;
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+
+        angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        playerRotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = playerRotation;
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            fired = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if(fired)
+        {
+            //rigidBody.velocity = new Vector2(0, 0);
+            rigidBody.AddForce(transform.right * -500);
+            fired = false;
+        }
+    }
+}
